16fbb7e49d8ae3e0c28e9acf447e42962422ee39
Yoana <yoanabarbie@icloud.com>
2024-06-08 19:25:23 +0200

    Made some code corrections to remove redundancy

--- js/main.js
+++ js/main.js
@@ -5,10 +5,8 @@ let screen = document.getElementById('screen');
 let infoScreen = document.getElementById('infoScreen');
 let chanelScreen = document.getElementById('chanelScreen');
 let tvInfo = document.getElementById('tv-info');
-
-// Creating array for the channels
 let channels = ['default', 'ch1', 'ch2', 'ch3', 'ch4', 'ch5', 'ch6', 'ch7', 'ch8', 'ch9'];
-let currentChannel = 0;
+let currentChannel = 0;  
 
 // Initially disable all buttons except power button
 for (let i = 0; i < buttons.length; i++) {
@@ -17,7 +15,7 @@ for (let i = 0; i < buttons.length; i++) {
     }
 }
 
-// Function for displaying the info and the current channel
+// Function for displaying the info and the current channel 
 let changeChannel = function(channelIndex) {
     document.getElementById(channels[currentChannel]).style.display = 'none';
     currentChannel = channelIndex;
@@ -26,66 +24,41 @@ let changeChannel = function(channelIndex) {
     infoScreen.innerHTML = "Date & Time: " + new Date().toLocaleString();
 }
 
-// Power button toggle function
-let togglePower = function() {
-    screen.classList.toggle('screenOff');
-    power.classList.toggle('ON');
-    power.classList.toggle('OFF');
-
-    if (!screen.classList.contains('screenOff')) {
-        for (let i = 0; i < buttons.length; i++) {
-            buttons[i].style.pointerEvents = "";
-        }
-        channels.forEach(function(channel) {
-            document.getElementById(channel).style.display = 'none';
-        });
-        changeChannel(0);
-    } else {
-        for (let i = 0; i < buttons.length; i++) {
-            if (buttons[i].id !== "butOnOff") {
-                buttons[i].style.pointerEvents = "none";
-            }
-        }
-        channels.forEach(function(channel) {
-            document.getElementById(channel).style.display = 'none';
-        });
-        infoScreen.innerHTML = "";
-        chanelScreen.innerHTML = "";
-        tvInfo.style.display = 'none';
-    }
-}
-
-// Channel change function wrapper
-let handleChannelChange = function(buttonId) {
-    let channelNum = parseInt(buttonId.replace('chan', ''));
-    changeChannel(channelNum);
-}
-
-// Info toggle function
-let toggleInfo = function() {
-    if (tvInfo.style.display === 'none') {
-        tvInfo.style.display = 'block';
-    } else {
-        tvInfo.style.display = 'none';
-    }
-}
-
-// Button actions mapping
-let buttonActions = {
-    'butOnOff': togglePower,
-    'info': toggleInfo
-};
-
-// Adding event listeners to buttons
+// Adding event listeners to buttons, toggle for power and led
 for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener('click', function(e) {
         let buttonId = e.target.id;
+        
+        if (buttonId === 'butOnOff') {
+            screen.classList.toggle('screenOff');
+            power.classList.toggle('ON');
+            power.classList.toggle('OFF');
 
-        // Check if the button has a predefined action
-        if (buttonActions[buttonId]) {
-            buttonActions[buttonId]();
+            if (!screen.classList.contains('screenOff')) {
+                for (let i = 0; i < buttons.length; i++) {
+                    buttons[i].style.pointerEvents = "";
+                }
+                for (let j = 0; j < channels.length; j++) {
+                    document.getElementById(channels[j]).style.display = 'none';
+                }
+                changeChannel(0);
+            } else {
+                for (let i = 0; i < buttons.length; i++) {
+                    if (buttons[i].id !== "butOnOff") {
+                        buttons[i].style.pointerEvents = "none";
+                    }
+                }
+                for (let j = 0; j < channels.length; j++) {
+                    document.getElementById(channels[j]).style.display = 'none';
+                }
+                infoScreen.innerHTML = "";
+                chanelScreen.innerHTML = "";
+                tvInfo.style.display = 'none';
+            }
         } else if (buttonId.startsWith('chan')) {
-            handleChannelChange(buttonId);
+            changeChannel(parseInt(buttonId.replace('chan', '')));
+        } else if (buttonId === 'info') {
+            tvInfo.style.display = (tvInfo.style.display === 'none') ? 'block' : 'none';
         }
     });
 }
